91b2c69a3a3c3ce922d52279cd4896b49a8f11e2
Gorang Madaan <ggorangmadaan@gmail.com>
2022-12-17 10:10:07 +0100

    routing commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Impressum from './Impressum'
+import Datenschutz from './Datenschutz'
 import './index.css';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Impressum",
+    element: <Impressum />,
+  },
+  {
+    path: "/Datenschutz",
+    element: <Datenschutz />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
